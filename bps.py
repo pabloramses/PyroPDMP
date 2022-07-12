d0680a3ad5ac776e7233ffb41cfd92ca2504c53b
pabloramses <pabloramses3@gmail.com>
2022-07-12 17:48:43 +0100

    Fixed velocity refresh
    Correctly implemented the random refresh of velocities.

--- bps.py
+++ bps.py
@@ -2,18 +2,17 @@ import numpy as np
 from utils import nhppSample
 
 class bps(nhppSample):
-    def __init__(self, gradient, bound, initial_pos, niter, int_bound = None, inv_int_bound = None, tol=10 ** -2):
+    def __init__(self, gradient, bound, initial_pos, niter, lr, int_bound = None, inv_int_bound = None, tol=10 ** -2):
         super(bps, self).__init__(1, inv_Lambda=inv_int_bound, Lambda=int_bound, tol=tol)
         self.d = len(initial_pos)
         self.gradient = gradient
         self.bound = bound
         self.initial_pos = initial_pos
         self.niter = niter
+        self.lr = lr
 
 
     def rate(self, time, pos, vel):
-        print("gradient", self.gradient(pos + time * vel))
-        print("coeff", np.dot(vel, self.gradient(pos + time * vel)))
         self.Rate = (np.dot(vel, self.gradient(pos + time * vel))>0)*np.dot(vel, self.gradient(pos + time * vel))
 
     def sample(self):
@@ -28,23 +27,38 @@ class bps(nhppSample):
         self.vel[:, 0] = np.random.multivariate_normal(np.zeros(self.d), np.eye(self.d), size=1)
 
         for i in range(1,self.niter):
-            if self.inv_Lambda:
-                upperTime = super().cinlar(x=self.pos[:,i], v=self.vel[:,i])
-            else:
-                upperTime = super().cinlar()
-            t = upperTime[0]
-            print(t)
-            self.times.append(t)
-            self.pos[:, i] = self.pos[:, i-1]+t*self.vel[:, i-1]
+            refresh = np.random.exponential(self.lr)
 
-            self.rate(t, self.pos[:, i], self.vel[:, i-1])
+            accepted = True
 
-            r = np.float32(self.Rate)
-            b = np.float32(self.bound(t, 0, self.pos[:, i], self.vel[:, i-1]))
-            u = np.random.random()
-            print("rate", r)
-            print("bound",b)
-            if u < (r/b):
-                self.vel[:, i] = self.vel[:,i-1] - 2*np.dot(((np.dot(self.vel[:,i-1],self.gradient(self.pos[:, i])))/(np.dot(self.gradient(self.pos[:, i]),self.gradient(self.pos[:, i])))),self.gradient(self.pos[:, i]))
-            else:
-                self.vel[:, i] = np.random.multivariate_normal(np.zeros(self.d), np.eye(self.d), size=1)
+            t = 0
+            while accepted:
+                prop_x = self.pos[:,i-1]
+                prop_v = self.vel[:,i-1]
+                if self.inv_Lambda:
+                    upperTime = super().cinlar(x=prop_x, v=prop_v)
+                else:
+                    upperTime = super().cinlar()
+                t = t + upperTime[0]
+                if t>refresh:
+                    print("refreshed")
+                    self.pos[:, i] = self.pos[:, i] + refresh * self.vel[:,i-1]
+                    self.vel[:, i] = np.random.multivariate_normal(np.zeros(self.d), np.eye(self.d))
+                    self.times.append(refresh)
+                    break
+
+                prop_x = prop_x+t*prop_v
+
+                self.rate(t, prop_x, prop_v)
+
+                r = np.float32(self.Rate)
+                b = np.float32(self.bound(t, 0, prop_x, prop_v))
+                u = np.random.random()
+                print("rate", r)
+                print("bound",b)
+                if u < (r/b):
+                    self.pos[:, i] = prop_x
+                    self.vel[:, i] = prop_v - 2*np.dot(((np.dot(prop_v,self.gradient(prop_x)))/(np.dot(self.gradient(prop_x),self.gradient(prop_x)))),self.gradient(prop_x))
+                    self.times.append(t)
+                    print("accepted")
+                    accepted = False
