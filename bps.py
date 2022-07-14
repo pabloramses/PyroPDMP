e702126b423ff112cf3e70151b232834a020c678
pabloramses <pabloramses3@gmail.com>
2022-07-14 08:13:09 +0100

    Clean

--- bps.py
+++ bps.py
@@ -20,9 +20,6 @@ class bps(nhppSample):
         d = len(self.initial_pos)
         self.pos = np.zeros((self.d, self.niter))
         self.vel = np.zeros((self.d, self.niter))
-        print(self.pos.shape)
-        print(self.initial_pos.shape)
-
         self.pos[:, 0] = self.initial_pos
         self.vel[:, 0] = np.random.multivariate_normal(np.zeros(self.d), np.eye(self.d), size=1)
 
@@ -33,16 +30,16 @@ class bps(nhppSample):
 
             t = 0
             while accepted:
-                prop_x = self.pos[:,i-1]
-                prop_v = self.vel[:,i-1]
+                prop_x = self.pos[:, i - 1]
+                prop_v = self.vel[:, i - 1]
+
                 if self.inv_Lambda:
                     upperTime = super().cinlar(x=prop_x, v=prop_v)
                 else:
                     upperTime = super().cinlar()
                 t = t + upperTime[0]
                 if t>refresh:
-                    print("refreshed")
-                    self.pos[:, i] = self.pos[:, i] + refresh * self.vel[:,i-1]
+                    self.pos[:, i] = self.pos[:, i-1] + refresh * self.vel[:,i-1]
                     self.vel[:, i] = np.random.multivariate_normal(np.zeros(self.d), np.eye(self.d))
                     self.times.append(refresh)
                     break
@@ -54,11 +51,8 @@ class bps(nhppSample):
                 r = np.float32(self.Rate)
                 b = np.float32(self.bound(t, 0, prop_x, prop_v))
                 u = np.random.random()
-                print("rate", r)
-                print("bound",b)
                 if u < (r/b):
                     self.pos[:, i] = prop_x
                     self.vel[:, i] = prop_v - 2*np.dot(((np.dot(prop_v,self.gradient(prop_x)))/(np.dot(self.gradient(prop_x),self.gradient(prop_x)))),self.gradient(prop_x))
                     self.times.append(t)
-                    print("accepted")
                     accepted = False
