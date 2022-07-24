cba7dab601eab52eb1abf0c5607ab6f1d86f454d
pabloramses <pabloramses3@gmail.com>
2022-07-24 11:41:49 +0100

    Example change

--- gaussian_2D_BPS.py
+++ gaussian_2D_BPS.py
@@ -2,7 +2,7 @@ import numpy as np
 from bps import bps
 import matplotlib.pyplot as plt
 Sigma = np.array([[1,-0.7],[-0.7,1]])
-Mu = np.array([10,7])
+Mu = np.array([100,70])
 Sigma_inv = np.linalg.inv(Sigma)
 def mult_gaussian_gradient(x):
     grad = np.zeros(len(x))
@@ -27,7 +27,7 @@ def inv_int_bound(t, x, v):
     return (-a + np.sqrt((a ** 2) + 2 * b * t)) / b
 
 initial_pos = np.array([0.5,1])
-sampler = bps(mult_gaussian_gradient, bound, initial_pos, niter = 100000, lr = 1, inv_int_bound= inv_int_bound)
+sampler = bps(mult_gaussian_gradient, bound, initial_pos, niter = 10000, lr = 1, inv_int_bound= inv_int_bound)
 sampler.sample()
 
 plt.plot(sampler.pos[0], sampler.pos[1])
