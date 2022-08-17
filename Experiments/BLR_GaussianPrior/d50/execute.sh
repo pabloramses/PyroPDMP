67caf498ced0442d01c69b249c591c3a877d30a2
pabloramses <pabloramses3@gmail.com>
2022-08-17 15:42:29 +0100

    .

--- Experiments/BLR_GaussianPrior/d50/execute.sh
+++ Experiments/BLR_GaussianPrior/d50/execute.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-python tune.py
+python3 GP_d50_n100_slar.py
+python3 GP_d50_n100_slow.py
+python3 GP_d50_n100_smed.py
+python3 GP_d50_n1000_slar.py
+python3 GP_d50_n1000_slow.py
+python3 GP_d50_n1000_smed.py
