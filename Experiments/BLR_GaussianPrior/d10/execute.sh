b6c3d9d5b69cbfd4d1222a7edd35f239545cc861
statrita2004 <statrita2004@gmail.com>
2022-08-17 12:02:23 +0100

    python to python3

--- Experiments/BLR_GaussianPrior/d10/execute.sh
+++ Experiments/BLR_GaussianPrior/d10/execute.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-python GP_d10_n100_slow.py
-python GP_d10_n100_smed.py
-python GP_d10_n100_slar.py
-python GP_d10_n1000_slow.py
-python GP_d10_n1000_smed.py
-python GP_d10_n1000_slar.py
+python3 GP_d10_n100_slow.py
+python3 GP_d10_n100_smed.py
+python3 GP_d10_n100_slar.py
+python3 GP_d10_n1000_slow.py
+python3 GP_d10_n1000_smed.py
+python3 GP_d10_n1000_slar.py
